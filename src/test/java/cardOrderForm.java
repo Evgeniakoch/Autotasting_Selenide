d7fbf441b94e52ab1579c1ca6f3ce54d9753a15f
Jane <evgenia.kochukova@gmail.com>
2023-08-28 21:12:47 +0300

    Tests modified

--- src/test/java/cardOrderForm.java
+++ src/test/java/cardOrderForm.java
@@ -6,9 +6,10 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.Keys.BACK_SPACE;
 
 
@@ -36,22 +37,6 @@ public class cardOrderForm {
         $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + TodayPlus3Days()),
                 Duration.ofSeconds(15)).shouldBe(visible);
     }
-
-    @Test
-    void shouldSendCardOrderFormWithUnvalidCity() {
-        Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999");
-        $("[data-test-id='city']").click();
-        $("[data-test-id='city']").find("[type=text]").setValue("Сан-Паулу");
-        $("[data-test-id='date']").find("[type=tel]").doubleClick().sendKeys(BACK_SPACE);
-        $("[data-test-id='date']").find("[type=tel]").setValue(TodayPlus3Days());
-        $("[data-test-id='name']").find("[type=text]").setValue("Иванов");
-        $("[data-test-id='phone']").find("[type=tel]").setValue("+79515255555");
-        $("[data-test-id='agreement']").click();
-        $(By.className("button")).click();
-        String expected = "Доставка в выбранный город недоступна";
-        String actual = $(By.cssSelector("[data-test-id='city'].input_invalid .input__sub")).getText();
-        assertEquals(expected, actual);
-    }
     
+
 }
